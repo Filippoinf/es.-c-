e0cd9e0cf0164355bed37e10ecf9050d74aadf5e
Filippoinf <123358532+Filippoinf@users.noreply.github.com>
2023-02-01 13:10:14 +0100

    AGGIUNTO FILE

---
+++ dati in input due vettori di numeri interi, dare in output un terzo vettore contenente le somme degli elementi corrispondenti.cpp
@@ -0,0 +1,16 @@
+/*
+
+    NOME: MAFFEI
+    COPYRIGHT: COLAMONICOCHIARULLI
+    AUTHOR: FILIPPO MAFFEI
+    DATE:28/11/2022 8:45
+    DESCRIPTION: Dati in input due vettori di numeri interi, dare in output un terzo vettore contenente le somme degli elementi corrispondenti
+*/
+#include <iostream>
+using namespace std
+int main ()
+    int 1;
+	int num=0
+	int vetttore[1]
+    int output
+	
